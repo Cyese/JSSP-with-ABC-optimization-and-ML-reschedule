0bcf2d4d163e106af21181c44203ce8c16d73d43
Cyese <87186561+Cyese@users.noreply.github.com>
2023-05-14 11:04:33 +0700

    Add module: class PhaseBaseSchedule for schedule.py class Bee add return value

---
+++ src/test.py
@@ -0,0 +1,16 @@
+"""
+    Module use for testing code
+"""
+
+# import numpy as np
+from schedule import PhaseBaseSchedule as PBSchedule
+
+# import random
+test1 = [1, 1, 1, 1, 2, 2, 3, 3]
+test2 = [4, 4, 4, 4, 5, 5, 5, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+P1 = [test1, test2]
+scheduler = PBSchedule(P1)
+result, cycle = scheduler.run()
+for item in result:
+    print(item)
+print(cycle)
